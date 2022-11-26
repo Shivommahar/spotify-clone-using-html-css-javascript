8f647877368695c4770cb73188d78e720c5588e9
Shivommahar <114341402+Shivommahar@users.noreply.github.com>
2022-11-26 21:29:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Spotify");
+let songs=[
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songname: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+];
+let audioElement=new Audio('1.mp3');
